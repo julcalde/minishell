f82f5f3a9fe28bfe4b88c118ea6cbdfe8841b544
julcalde <julcalde@student.42heilbronn.de>
2025-03-17 17:46:24 +0100

    added functions to parse input and handle errors

--- parser/parser.c
+++ parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: julcalde <julcalde@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 17:39:47 by julcalde          #+#    #+#             */
-/*   Updated: 2025/03/17 17:41:17 by julcalde         ###   ########.fr       */
+/*   Updated: 2025/03/17 17:45:12 by julcalde         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,4 +19,15 @@ t_ast	*parse_input(char **tokens)
 	ast = malloc(sizeof(t_ast));
 	if (!ast)
 		perr_exit("malloc ast failed\n");
+	ast->command = ft_strdup(tokens[0]);
+	ast->args = tokens;
+	ast->left = NULL;
+	ast->right = NULL;
+	return (ast);
+}
+
+void	handle_err(t_ast *ast)
+{
+	if (!ast || !ast->command)
+		perr_exit("parsing error\n");
 }

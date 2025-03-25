38ede1b5c68f07b05dca9d8820aa202dac48029c
julcalde <julcalde@student.42heilbronn.de>
2025-03-25 18:28:27 +0100

    needed also strjoin

--- parser/libft_utils_moar.c
+++ parser/libft_utils_moar.c
@@ -6,7 +6,7 @@
 /*   By: julcalde <julcalde@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 17:10:28 by julcalde          #+#    #+#             */
-/*   Updated: 2025/03/25 17:12:04 by julcalde         ###   ########.fr       */
+/*   Updated: 2025/03/25 18:27:00 by julcalde         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,4 +53,31 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	}
 	dst[dstlen + i] = '\0';
 	return (dstlen + srclen);
+}
+
+/* Check 'man strjoin' */
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*s3;
+	size_t	s1_len;
+	size_t	s2_len;
+	size_t	c1;
+	size_t	c2;
+
+	c1 = 0;
+	c2 = 0;
+	if (!s1 || !s2)
+		return (NULL);
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	s3 = (char *) malloc((s1_len + s2_len + 1) * sizeof(char));
+	if (!s3)
+		return (NULL);
+	while (s1[c1])
+		s3[c2++] = s1[c1++];
+	c1 = 0;
+	while (s2[c1])
+		s3[c2++] = s2[c1++];
+	s3[c2] = '\0';
+	return (s3);
 }

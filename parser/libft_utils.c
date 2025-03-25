8b47bb46964756d24f5c3450bc8465b0ad471fb9
julcalde <julcalde@student.42heilbronn.de>
2025-03-25 16:34:41 +0100

    added custom strchr and strtok to necessary libft functions

--- parser/libft_utils.c
+++ parser/libft_utils.c
@@ -6,7 +6,7 @@
 /*   By: julcalde <julcalde@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/14 16:46:23 by julcalde          #+#    #+#             */
-/*   Updated: 2025/03/20 19:55:14 by julcalde         ###   ########.fr       */
+/*   Updated: 2025/03/25 16:33:52 by julcalde         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,6 +43,7 @@ char	*ft_strdup(const char *s1)
 	dupe[c] = '\0';
 	return (dupe);
 }
+
 /* Check 'man strcmp' */
 int	ft_strcmp(char *s1, char *s2)
 {
@@ -52,4 +53,50 @@ int	ft_strcmp(char *s1, char *s2)
 	while (s1[i] && s2[i] && (s1[i] == s2[i]))
 		i++;
 	return (s1[i] - s2[i]);
+}
+
+/* Check 'man strchr' */
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == (char)c)
+			return ((char *) &s[i]);
+		i++;
+	}
+	if ((char)c == '\0')
+		return ((char *) &s[i]);
+	return (NULL);
+}
+
+/* Check 'man strtok' */
+char	*ft_strtok(char *str, const char *delim)
+{
+	static char	*save_p;
+	char		*start;
+	char		*end;
+
+	if (str)
+		save_p = str;
+	if (!save_p || !*save_p)
+		return (NULL);
+	start = save_p;
+	while (*start && ft_strchr(delim, *start))
+		start++;
+	if (!*start)
+		return (save_p = NULL, NULL);
+	end = start;
+	while (*end && !ft_strchr(delim, *end))
+		end++;
+	if (*end)
+	{
+		*end = '\0';
+		save_p = end + 1;
+	}
+	else
+		save_p = end;
+	return (start);
 }

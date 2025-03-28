1afcc225bee38293d9d6c98e0c2fc0ef1a41ff74
Fileonar <fileonar@student.42heilbronn.de>
2025-03-28 17:58:26 +0100

    testing echo, bringing it home

--- exec/builtins.c
+++ exec/builtins.c
@@ -6,7 +6,7 @@
 /*   By: fileonar <fileonar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/20 20:34:33 by fileonar          #+#    #+#             */
-/*   Updated: 2025/03/28 17:46:18 by fileonar         ###   ########.fr       */
+/*   Updated: 2025/03/28 17:57:34 by fileonar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,25 +23,24 @@
 
 int	ft_echo(t_ast *ast)
 {
-	//check if it is echo or echo -n
-	//check if there is printable characters in the string
-	//if there is spaces, skip until the first character
-	//print the string
-	// printf("args: %s\n", ast);
-	
-	// while (ast == ' ')
-	// 	i++;
-	// if (ast == '-' && ast == 'n')
-	// {
-	// 	i += 2;
-	// 	while (ast == ' ')
-	// 		i++;
-	// 	while (ast)
-	// 	{
-	// 		write(1, &ast, 1);
-	// 		i++;
-	// 	}
-	// }
-	printf("ast: %s\n", ast->args[1]);
+	if (ast->args[1] && !ft_strcmp(ast->args[1], "-n"))
+	{
+		while (*ast->args != NULL)
+		{
+			printf("%s ", ast->args[1]);
+			ast->args++;
+		}
+	}
+	else if (!ast->args[1])
+		printf("\n");
+	else
+	{
+		while (*ast->args != NULL)
+		{
+			printf("%s ", ast->args[1]);
+			ast->args++;
+		}
+		printf("\n");
+	}
 	return (EXIT_SUCCESS);
 }

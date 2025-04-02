f22868e289edb66416d92c5361e0c6a1b806705d
julcalde <julcalde@student.42heilbronn.de>
2025-04-02 21:15:22 +0200

    added new functions

--- parser/parser.h
+++ parser/parser.h
@@ -6,7 +6,7 @@
 /*   By: julcalde <julcalde@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/14 16:32:15 by julcalde          #+#    #+#             */
-/*   Updated: 2025/04/02 21:51:54 by julcalde         ###   ########.fr       */
+/*   Updated: 2025/04/02 21:52:34 by julcalde         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -86,68 +86,85 @@ typedef struct s_ast
 
 // CLEANER.C: clean-up function for freeing memory at the end.
 
-void	cleanup(t_env *env, t_ast *ast, t_token **tokens);
-void	cleaner_env(t_env *env);
-void	cleaner_ast(t_ast *ast);
+void			cleanup(t_env *env, t_ast *ast, t_token **tokens);
+void			cleaner_env(t_env *env);
+void			cleaner_ast(t_ast *ast);
 
 // EXEGGCUTE_UTILS.C: Utility functions for executing commands.
 
-int		is_executable(char *path);
-char	*join_path(char *dir, char *cmd);
-char	*get_env_value(char *key, t_env *env);
-char	*get_path(char *cmd, t_env *env);
-char	**env_to_array(t_env *env);
+int				is_executable(char *path);
+char			*join_path(char *dir, char *cmd);
+char			*get_env_value(char *key, t_env *env);
+char			*get_path(char *cmd, t_env *env);
+char			**env_to_array(t_env *env);
 
 // EXEGGUTOR.C: Functions for executing commands.
 
-int		is_builtin(char *command);
-int		validate_builtin(t_ast *ast);
-void	exec_builtin(t_ast *ast, t_env *env);
-void	external_cmd_exe(t_ast *ast, t_env *env);
-void	exec_cmd(t_ast *ast, t_env *env);
+int				is_builtin(char *command);
+int				validate_builtin(t_ast *ast);
+void			exec_builtin(t_ast *ast, t_env *env);
+void			external_cmd_exe(t_ast *ast, t_env *env);
+void			exec_cmd(t_ast *ast, t_env *env);
 
 // INIT.C: Initializes the shell environment.
 
-void	init_env(t_env **env, char **envp);
+void			init_env(t_env **env, char **envp);
 
 // INPUT.C: Functions for reading and handling user input.
 
-char	*read_input(void);
-void	add_to_history(char *input);
+char			*read_input(void);
+void			add_to_history(char *input);
 
 // LOOP.C: Core minishell loop.
 
-void	shell_loop(t_env *env);
+void			shell_loop(t_env *env);
 
 // PARSER.C: Parses tokens into an Abstract Syntax Tree (AST)
 
-t_ast	*parse_input(char **tokens);
-void	handle_err(t_ast *ast);
+t_ast			*parse_input(char **tokens);
+void			handle_err(t_ast *ast);
+
+// QUOTE_HANDLER.C: Functions for handling quotes.
+t_token_type	get_quote_type(char c);
+int				validate_quotes(char *input);
 
 // SIGNALS.C: Functions for handling signals.
 
-void	handle_sigint(int sig);
-void	handle_sigquit(int sig);
-void	set_sigs(void);
+void			handle_sigint(int sig);
+void			handle_sigquit(int sig);
+void			set_sigs(void);
 
 // TOKENIZER.C: Functions for tokenizing user input.
 
-char	**tokenize_input(char *input);
+char			**tokenize_input(char *input);
+
+// TOKENIZER_UTILS.C: Utility functions for tokenizing.
+
+char			*get_next_token(char **input);
+
+// TOKENIZER_CHECKS.C: Functions for checking token types.
+
+int				is_whitespace(char c);
+int				is_special(char c);
+int				is_redirect(int type);
 
 // UTILS.C: Utility functions.
 
-void	perr_exit(char *err_msg);
-void	ft_free_array(char **array);
+void			perr_exit(char *err_msg);
+void			ft_free_array(char **array);
 
-/* Helper functions libft_utils.c and libft_utils_moar.c */
+/* Helper functions libft_utils.c,libft_utils_moar.c and libft_utils_moaar.c */
 
-size_t	ft_strlen(const char *s);
-char	*ft_strdup(const char *s1);
-int		ft_strcmp(char *s1, char *s2);
-char	*ft_strchr(const char *s, int c);
-char	*ft_strtok(char *str, const char *delim);
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
-size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
-char	*ft_strjoin(char const *s1, char const *s2);
+size_t			ft_strlen(const char *s);
+char			*ft_strdup(const char *s1);
+int				ft_strcmp(char *s1, char *s2);
+char			*ft_strchr(const char *s, int c);
+char			*ft_strtok(char *str, const char *delim);
+size_t			ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t			ft_strlcat(char *dst, const char *src, size_t dstsize);
+char			*ft_strjoin(char const *s1, char const *s2);
+void			*ft_calloc(size_t count, size_t size);
+void			*ft_memcpy(void *dst, const void *src, size_t n);
+char			*ft_substr(const char *s, unsigned int start, size_t len);
 
 #endif

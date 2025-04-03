cd0bdb3dfdd78b82de8043ed2ecdd2dd9108a813
julcalde <julcalde@student.42heilbronn.de>
2025-04-03 15:53:58 +0200

    added check for pipe or redirs and fixed param type

--- parser/syntax_utils.c
+++ parser/syntax_utils.c
@@ -6,17 +6,21 @@
 /*   By: julcalde <julcalde@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/02 22:44:08 by julcalde          #+#    #+#             */
-/*   Updated: 2025/04/02 22:47:47 by julcalde         ###   ########.fr       */
+/*   Updated: 2025/04/03 15:52:21 by julcalde         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parser.h"
 
 /* Returns 1 if the token type is a redirection, 0 otherwise */
-int	is_redirect(int type)
+int	is_redirect(t_token_type type)
 {
 	return (type == T_REDIRECT_IN || type == T_REDIRECT_OUT || \
 			type == T_REDIRECT_APPEND);
 }
 
-
+/* Returns 1 if the token type is a pipe or a redirection, 0 otherwise */
+int	is_operator(t_token_type type)
+{
+	return (type == T_PIPE || is_redirect(type));
+}

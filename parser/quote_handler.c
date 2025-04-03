49312a2585a453ccf37a75128072c2cb7d8b7300
julcalde <julcalde@student.42heilbronn.de>
2025-04-03 18:57:34 +0200

    norminette

--- parser/quote_handler.c
+++ parser/quote_handler.c
@@ -6,7 +6,7 @@
 /*   By: julcalde <julcalde@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/01 23:58:22 by julcalde          #+#    #+#             */
-/*   Updated: 2025/04/02 23:07:17 by julcalde         ###   ########.fr       */
+/*   Updated: 2025/04/03 18:56:59 by julcalde         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,16 +26,19 @@
 int	validate_quotes(char *input)
 {
 	int		i;
-	char	open_quote;
+	char	quote;
 
 	i = -1;
 	while (input[++i])
 	{
-		if (input[i] == '\'' || input[i] == '"')
+		if (input[i] == '\\' && input[i + 1])
+			i++;
+		else if (input[i] == '\'' || input[i] == '"')
 		{
-			open_quote = input[i];
-			while (input[++i] && input[i] != open_quote)
-				;
+			quote = input[i];
+			while (input[++i] && input[i] != quote)
+				if (quote == '"' && input[i] == '\\' && input[i + 1])
+					i++;
 			if (!input[i])
 				return (0);
 		}
@@ -53,5 +56,7 @@ int	validate_token_quotes(t_token *token)
 		return (1);
 	str = token->value;
 	len = ft_strlen(str);
-	return (len >= 2 && str[0] == str[len - 1]);
+	if (len < 2 || str[0] != str[len - 1])
+		return (0);
+	return (1);
 }

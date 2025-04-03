179d0d81e54e10a11ba607be47cecb7cbda24a6e
julcalde <julcalde@student.42heilbronn.de>
2025-04-03 19:01:16 +0200

    rejection of consecutive pipes, ensurance for redirs followed by T_WORD

--- parser/syntax_checker.c
+++ parser/syntax_checker.c
@@ -6,33 +6,40 @@
 /*   By: julcalde <julcalde@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/01 23:58:36 by julcalde          #+#    #+#             */
-/*   Updated: 2025/04/02 23:22:06 by julcalde         ###   ########.fr       */
+/*   Updated: 2025/04/03 18:52:47 by julcalde         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parser.h"
 
-// Returns 1 if the token type is a redirection, 0 otherwise
 static int	validate_operator(t_token **tokens, int i)
 {
-	if (tokens[i] == T_PIPE && (i == 0 || !tokens[i + 1]))
+	if (!tokens[i] || !tokens[i + 1])
 		return (0);
-	if (is_redirect(tokens[i]->type) && \
-	(!tokens[i + 1] || tokens[i + 1]->type != T_WORD))
+	if (tokens[i]->type == T_PIPE && (i == 0 || tokens[i + 1]->type == T_PIPE))
+		return (0);
+	if (is_redirect_type(tokens[i]->type) && tokens[i + 1]->type != T_WORD)
 		return (0);
 	return (1);
 }
 
-// Validate that the token has a matching quote
+
 int	validate_syntax(t_token **tokens)
 {
 	int	i;
 
+	if (!tokens || !*tokens)
+		return  (0);
 	i = -1;
 	while (tokens[++i])
 	{
-		if (!validate_operator(tokens, i) || !validate_token_quotes(tokens[i]))
+		if (!validate_token_quotes(tokens[i]))
 			return (0);
+		if (is_operator_type(tokens[i]->type) && !validate_operator(tokens, i))
+		{
+			handle_err(NULL); // or "syntax error near unexpected token '<token>' \n"
+			return (0);
+		}
 	}
 	return (1);
 }
